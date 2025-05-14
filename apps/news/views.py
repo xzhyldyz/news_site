f980dbef3ae4f01cfe8132d6d8170d24ac9a79a2
xzhyldyz <xzhldz@gmail.com>
2025-05-14 18:59:27 +0600

    попытка 1

--- apps/news/views.py
+++ apps/news/views.py
@@ -4,10 +4,17 @@ from .models import Article, Category
 def homepage(request):
     articles = Article.objects.all()
     sliders = Article.objects.all()[:3]
-    category = Category.objects.all()[:8]
-    context = {                         #передача данные через контекст
+    category = Category.objects.all()
+    context = {                       
         'articles': articles,
         'sliders': sliders,
         'category': category,
         }
     return render(request, 'index.html', context)   # Передача данных из админки в шаблон
+
+def sport_category(request):
+    sport_category = Category.objects.get(name = 'спорт')
+    articles = Article.objects.filter(category=sport_category)
+    
+    return render(request, 'pages/news-category-sport.html', {'articles': articles})
+
--- news_site/urls.py
+++ news_site/urls.py
@@ -2,11 +2,12 @@ from django.contrib import admin
 from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
-from apps.news.views import homepage
+from apps.news.views import homepage, sport_category
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', homepage, name="home"),
+    path('category/sport/', sport_category, name="sport_category"),
 ]
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)     
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)    #Подключение статики
